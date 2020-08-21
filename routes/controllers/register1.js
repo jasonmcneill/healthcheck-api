baf6c134c9035c060c5c73c578c220dfbfb216a9
Jason McNeill <vrtjason@gmail.com>
2020-08-20 18:11:16 -0700

    Update member query; send registration link

--- routes/controllers/register1.js
+++ routes/controllers/register1.js
@@ -14,10 +14,12 @@ exports.POST = (req, res) => {
   }
 
   const sqlCheckUserExists = `
-    SELECT memberid, email, smsphone
-    FROM members
-    WHERE email = ?
-    OR smsphone = ?
+    SELECT m.memberid, m.fullname, m.email, m.smsphone, m.lang, mr.role
+    FROM members m
+    INNER JOIN members__roles mr ON m.memberid = mr.memberid
+    WHERE m.email = ?
+    OR m.smsphone = ?
+    LIMIT 1;
   `;
   db.query(sqlCheckUserExists, [email, smsphone], (error, result) => {
     if (error) {
@@ -32,11 +34,70 @@ exports.POST = (req, res) => {
         .status(404)
         .send({ msg: "member not found", msgType: "error" });
     }
-    // TODO:  Send registration link either by SMS or by e-mail
-    return res.status(200).send({
-      msg: "registration link sent",
-      msgType: "success",
-      member: result[0],
-    });
+
+    const fullname = result[0].fullname;
+    const email = result[0].email;
+    const smsphone = result[0].smsphone;
+    const lang = result[0].lang;
+    const role = result[0].role;
+    const memberid = result[0].memberid;
+    const registrationToken = require("crypto").randomBytes(16).toString("hex");
+    const sqlAddRegistrationToken = `
+      UPDATE members
+      SET registrationToken = ?
+      WHERE memberid = ?
+    `;
+    db.query(
+      sqlAddRegistrationToken,
+      [memberid, registrationToken],
+      (error, result) => {
+        if (error) {
+          return res.status(400).send({
+            msg: "unable to add registration token",
+            msgType: "error",
+            error: error,
+          });
+        }
+        const registrationUrl =
+          process.env.ENVIRONMENT === "production"
+            ? `https://firstprinciples.mobi/lang/${lang}/healthcheck/#/register/${token}`
+            : `http://localhost:3000/#/register/${token}`;
+
+        const emailSubject = "Complete your registration for Health Check";
+
+        const emailBody = `
+          <section id="usd21HealthCheckRegistration">
+            <p>
+              This message is for ${fullname}.  Please complete your registration for the Health Check app by clicking on the link below:
+            </p>
+
+            <p>
+              <strong><a href="${registrationUrl}">Register for Health Check</a></strong>
+            </p>
+
+            <p>
+              Sincerely,
+            </p>
+
+            <p>
+              The Cyberministry
+            </p>
+          </section>
+        `.trim();
+
+        const sgMail = require("@sendgrid/mail");
+        sgMail.setApiKey(process.env.HEALTH_CHECK_SENDGRID_API_KEY);
+        const msg = {
+          to: `${fullname} <${email}>`,
+          from: "Health Check <jason.mcneill@grindstonewebdev.com>",
+          subject: `${emailSubject}`,
+          html: `${emailBody}`,
+        };
+        sgMail.send(msg);
+        return res
+          .status(200)
+          .send({ msg: "registration link sent", msgType: "success" });
+      }
+    );
   });
 };
