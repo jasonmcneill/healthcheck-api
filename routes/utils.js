052e59c9cc2e470aed0d88b26fabfaa75e400f34
Jason McNeill <vrtjason@gmail.com>
2020-08-23 22:22:25 -0700

    Abstract sending of SMS and e-mail

--- routes/utils.js
+++ routes/utils.js
@@ -20,3 +20,43 @@ exports.authenticateToken = (req, res, next) => {
     }
   );
 };
+
+exports.sendSms = (recipient, content) => {
+  const twilio = require("twilio");
+  const client = new twilio(
+    process.env.HEALTH_CHECK_TWILIO_ACCOUNT_SID,
+    process.env.HEALTH_CHECK_TWILIO_AUTH_TOKEN
+  );
+  let sender = "+12058518089";
+  return new Promise((resolve, reject) => {
+    client.messages
+      .create({
+        from: sender,
+        to: recipient,
+        body: content,
+      })
+      .then((message) => {
+        console.log(require("util").inspect(message, true, 7, true));
+        resolve(message);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+exports.sendEmail = (recipient, sender, subject, body) => {
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.HEALTH_CHECK_SENDGRID_API_KEY);
+  const msg = {
+    to: recipient,
+    from: sender,
+    subject: subject,
+    html: body,
+  };
+  return new Promise((resolve, reject) => {
+    resolve(sgMail.send(msg));
+  }).catch((error) => {
+    reject(error);
+  });
+};
