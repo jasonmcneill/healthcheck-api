e847efd4ea11a21c6caca94e6ba7cd0e0f56ab62
Jason McNeill <vrtjason@gmail.com>
2020-08-29 15:57:50 -0700

    Add SMS token generator

--- routes/utils.js
+++ routes/utils.js
@@ -112,3 +112,8 @@ exports.validatePhone = (number, countryCode) => {
   };
   return returnObject;
 }
+
+exports.smsToken = () => {
+  const token = Math.floor(100000 + Math.random() * 900000);
+  return token;
+}
